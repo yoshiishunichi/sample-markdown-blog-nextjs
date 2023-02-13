615dd955216fb05ea786b3f6398f3b4f8f89501b
吉井駿一 <syun_syun01@icloud.com>
2023-02-13 21:33:39 +0900

    :sparkles: Blogの詳細ページ作成

---
+++ src/pages/blogs/[slug].tsx
@@ -0,0 +1,42 @@
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+
+import { getAllSlug } from "libs/get-all-slug";
+import { getMarkdown } from "libs/get-markdown";
+import { markdownToHtml } from "libs/markdown-to-html";
+
+type BlogDetailPageProps = {
+  htmlContent: string;
+};
+
+const BlogDetailPage: NextPage<BlogDetailPageProps> = ({ htmlContent }) => {
+  return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
+};
+
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    fallback: false,
+    paths: getAllSlug("contents").map((slug) => {
+      return {
+        params: {
+          slug,
+        },
+      };
+    }),
+  };
+};
+
+export const getStaticProps: GetStaticProps<BlogDetailPageProps> = async ({ params }) => {
+  const slug = params?.slug;
+  if (!slug)
+    return {
+      notFound: true,
+    };
+
+  return {
+    props: {
+      htmlContent: await markdownToHtml(getMarkdown(`contents/${slug}.md`).content),
+    },
+  };
+};
+
+export default BlogDetailPage;
