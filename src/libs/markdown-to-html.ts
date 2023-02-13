becf0b0abc4a54eabf9063f251cb56157c39ad0a
吉井駿一 <syun_syun01@icloud.com>
2023-02-13 23:40:40 +0900

    :sparkles: 生のhtmlタグを変換する処理を追加

--- src/libs/markdown-to-html.ts
+++ src/libs/markdown-to-html.ts
@@ -1,13 +1,19 @@
+import rehypeRaw from "rehype-raw";
+import rehypeStringify from "rehype-stringify";
 import { remark } from "remark";
 import remarkBreaks from "remark-breaks";
 import remarkGfm from "remark-gfm";
-import remarkHtml from "remark-html";
+import remarkRehype from "remark-rehype";
 
 export const markdownToHtml = async (markdownContent: string) => {
   const result = await remark()
     .use(remarkGfm)
     .use(remarkBreaks)
-    .use(remarkHtml)
+    .use(remarkRehype, {
+      allowDangerousHtml: true,
+    })
+    .use(rehypeRaw)
+    .use(rehypeStringify)
     .process(markdownContent);
   return result.toString();
 };
