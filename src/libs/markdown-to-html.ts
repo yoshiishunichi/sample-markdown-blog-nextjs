bbcd1395808051683ad279fd022bab129f07932a
吉井駿一 <syun_syun01@icloud.com>
2023-02-13 22:55:40 +0900

    :sparkles: remarkGfm を追加

--- src/libs/markdown-to-html.ts
+++ src/libs/markdown-to-html.ts
@@ -1,7 +1,8 @@
 import { remark } from "remark";
+import remarkGfm from "remark-gfm";
 import remarkHtml from "remark-html";
 
 export const markdownToHtml = async (markdownContent: string) => {
-  const result = await remark().use(remarkHtml).process(markdownContent);
+  const result = await remark().use(remarkGfm).use(remarkHtml).process(markdownContent);
   return result.toString();
 };
