2e6badaff1d098394c8b671c57b0d65de016e6f3
吉井駿一 <syun_syun01@icloud.com>
2023-02-13 23:15:35 +0900

    :sparkles: br タグに対応

--- src/libs/markdown-to-html.ts
+++ src/libs/markdown-to-html.ts
@@ -1,8 +1,13 @@
 import { remark } from "remark";
+import remarkBreaks from "remark-breaks";
 import remarkGfm from "remark-gfm";
 import remarkHtml from "remark-html";
 
 export const markdownToHtml = async (markdownContent: string) => {
-  const result = await remark().use(remarkGfm).use(remarkHtml).process(markdownContent);
+  const result = await remark()
+    .use(remarkGfm)
+    .use(remarkBreaks)
+    .use(remarkHtml)
+    .process(markdownContent);
   return result.toString();
 };
